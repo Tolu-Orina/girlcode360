42514e0d71f26573560508c02f967384a3a40a0f
Tolulope Orina <toluorina@gmail.com>
2026-08-17 15:14:51 +0100

    Updating girlcode360

--- apps/web/src/components/blocks/marketing-layout.ts
+++ apps/web/src/components/blocks/marketing-layout.ts
@@ -1,3 +1,3 @@
-export const marketingPad = "w-full px-16";
-export const marketingChromePad = "w-full px-6";
-export const marketingHeroPad = "w-full px-10";
+export const marketingPad = "w-full px-6 lg:px-16";
+export const marketingChromePad = "w-full px-4 lg:px-6";
+export const marketingHeroPad = "w-full px-6 lg:px-10";
